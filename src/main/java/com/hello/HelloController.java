6fd4c910e9680ef51c9b26e1432f653668570142
zemoso <vasanth.kumar@zemosolabs.com>
2018-05-09 21:13:46 +0530

    add basic controller test

---
+++ src/main/java/com/hello/HelloController.java
@@ -0,0 +1,17 @@
+package com.hello;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public @ResponseBody
+    String greeting() {
+        return "Hello World";
+    }
+
+}
---
+++ src/main/java/com/test/ControllerTest.java
@@ -0,0 +1,23 @@
+package com.test;
+
+
+import com.hello.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+public class ControllerTest {
+
+    private HelloController controller;
+
+    @Test
+    public void contexLoads() throws Exception {
+        controller = new HelloController();
+        assertThat(controller).isNotNull();
+    }
+}
